06e64b77c760b8037c65fe347124eb8f789a8435
Scream <arnelvergara1234@gmail.com>
2026-08-18 11:25:05 +0800

    Update routes to include Login and Userlists

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 import Userlists from './pages/Userlists';
 import Userinput from './pages/UserInput';
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Userlists />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/userlists" element={<Userlists />} />
         <Route path="/add" element={<Userinput />} />
       </Routes>
     </HashRouter>
